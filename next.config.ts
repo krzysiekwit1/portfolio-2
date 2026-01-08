c1d1fef9f276888635c50bab1584a6c582cd3f2a
krzysiekwit2 <krzysiekwit1@gmail.com>
2026-01-08 18:04:22 +0100

    inial commit for deploying

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio-2' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-2' : '',
+    images: {unoptimized: true},
+    trailingSlash: true
+}
 
-export default nextConfig;
+export default nextConfig
